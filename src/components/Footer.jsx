c9d17de8137f93f57a08885cc5a4c87c31110520
Atila <kassy.loulou61@gmail.com>
2026-05-29 22:08:19 +0200

    Footer, Page contact, backend pour les mails

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,27 @@
-import "./scss components/Footer.scss"
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import "./scss components/Footer.scss";
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
+        {/* Branding */}
         <div className="footer-section">
-          <h3>Mon Site</h3>
-          <p>© 2026 Tous droits réservés.</p>
+          <h3>Mon Portfolio</h3>
+          <p>Développeur web passionné</p>
+          <p>© 2026 Tous droits réservés</p>
         </div>
-
+        {/* Réseaux */}
         <div className="footer-section">
           <h4>Réseaux</h4>
-          <a href="#">Instagram</a>
-          <a href="#">Twitter</a>
-          <a href="#">LinkedIn</a>
-        </div>
 
+          <a href="https://github.com/Atila18" target="_blank" rel="noreferrer">
+            <FaGithub /> Github
+          </a>
+          <a href="https://profile.indeed.com/?hl=fr_FR&co=FR&from=gnav-homepage" target="_blank" rel="noreferrer">
+            <FaGoogle /> Indeed
+          </a>
+        </div>
       </div>
     </footer>
   );

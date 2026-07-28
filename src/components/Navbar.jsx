08d131a66c0bcf418477f769b62b00e72cf55095
Atila18 <kassy.loulou61@gmail.com>
2026-07-28 13:04:02 +0200

    Font style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 import './scss components/Navbar.scss'
 import Logo from './logo/Logo.png'
+import ('https://fonts.googleapis.com/css2?family=Imperial+Script&display=swap')
 
 
 function Navbar() {
